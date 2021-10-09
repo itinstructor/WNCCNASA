2f7fc7cfcd9b3d1c26ce1dc1d5c20a94f5d0b067
itinstructor <32806514+itinstructor@users.noreply.github.com>
2021-10-09 09:35:54 -0600

    Update Code Examples

--- GoPiGo Examples/motors.py
+++ GoPiGo Examples/motors.py
@@ -27,37 +27,42 @@ gpg = easy.EasyGoPiGo3()
 
 
 def main():
-    print("Current speed: " + str(gpg.get_speed()))
-    time.sleep(2)
-    
+    SLEEP_2_SECOND = 2
+    SLEEP_1_SECOND = 1
+
+    # Print the current speed
+    # The default speed is 300
+    print(str(gpg.get_speed()))
+    time.sleep(SLEEP_2_SECOND)
+
     print("Move the motors forward freely for 2 second.")
     gpg.forward()
-    time.sleep(2)
+    time.sleep(SLEEP_2_SECOND)
     # Stop both motors
     gpg.stop()
 
     print("Stop the motors for 1 second.")
-    time.sleep(1)
+    time.sleep(SLEEP_2_SECOND)
 
-    print("Drive the motors 5 inches and then stop.")
-    gpg.drive_inches(5, True)
-    time.sleep(1)
+    print("Drive the motors 12 inches and then stop.")
+    gpg.drive_inches(12, True)
+    time.sleep(SLEEP_2_SECOND)
 
     print("Turn right 1 second.")
     gpg.right()
-    time.sleep(1)
+    time.sleep(SLEEP_1_SECOND)
 
     print("Turn left 1 second.")
     gpg.left()
-    time.sleep(1)
+    time.sleep(SLEEP_1_SECOND)
 
     print("Spin left.")
     gpg.spin_left()
-    time.sleep(1)
+    time.sleep(SLEEP_1_SECOND)
 
     print("Spin right.")
     gpg.spin_right()
-    time.sleep(1)
+    time.sleep(SLEEP_1_SECOND)
 
     print("Turn left 90 degrees")
     gpg.turn_degrees(-90)
@@ -71,4 +76,3 @@ def main():
 
 
 main()
-
