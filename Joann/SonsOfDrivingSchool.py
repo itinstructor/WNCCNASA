815845e35915dcc1efb92ed4c1dca235b3409fc5
jonesj95wncc <80473876+jonesj95wncc@users.noreply.github.com>
2021-10-19 14:23:40 -0600

    Update SonsOfDrivingSchool.py

--- Joann/SonsOfDrivingSchool.py
+++ Joann/SonsOfDrivingSchool.py
@@ -13,3 +13,20 @@ from easygopigo3 import EasyGoPiGo3
 # GPG is the GoPiGo3 object used to access methods and properties
 gpg = EasyGoPiGo3()
 
+#-------------------------Stage 1--------------------------------#
+# Square
+def square(distance):
+    # Drive a right square based on the distance argument
+    # Loop four times, Loop starts at 0,
+    # Ends at 1 less than the last number
+    # The loop increments 0, 1, 2, 3
+    print("Make a square")
+    for x in range(0, 4):
+        # Print the loop counter
+        print(x)
+        gpg.led_off("right")
+        gpg.drive_inches(
+            distance,       # How far to drive in inches
+            True            # Blocking, nothing else can happen while moving
+        )
+
