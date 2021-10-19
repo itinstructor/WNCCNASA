84efcb424d4208e6e4fbc254c3fe74b3bf1e7d28
jonesj95wncc <80473876+jonesj95wncc@users.noreply.github.com>
2021-10-19 14:16:00 -0600

    Update SonsOfDrivingSchool.py

---
+++ Joann/SonsOfDrivingSchool.py
@@ -0,0 +1,15 @@
+"""
+    Name: sons_of_driving_school.py
+    Author:
+    Created: 10-19-2021
+    Purpose: 
+"""
+# Import the time library for the sleep function
+import time
+# Import GoPiGo3 library
+from easygopigo3 import EasyGoPiGo3
+
+# Create an instance of the GoPiGo3 class
+# GPG is the GoPiGo3 object used to access methods and properties
+gpg = EasyGoPiGo3()
+
