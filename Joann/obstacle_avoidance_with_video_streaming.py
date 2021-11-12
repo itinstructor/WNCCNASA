30436627949ba15027c64bab83a1ab91b58606ea
jonesj95wncc <80473876+jonesj95wncc@users.noreply.github.com>
2021-11-12 12:44:31 -0700

    obstacle avoidance with servo

--- Joann/obstacle_avoidance_with_video_streaming.py
+++ Joann/obstacle_avoidance_with_video_streaming.py
@@ -13,33 +13,34 @@ servo = gpg.init_servo("SERVO1")
 # But technically, I2C1 and I2C2 are the same thing, so we don't have to pass any port to the constructor.
 my_distance_sensor = gpg.init_distance_sensor()
 
+# Read the sensor into variables
+#mm = str(my_distance_sensor.read_mm())
+inches = str(my_distance_sensor.read_inches())
+distanceInches = float(inches)
+
+# Print the values of the sensor to the console
+print("Distance Sensor Reading: " +
+      format(distanceInches) + " inches ")  # + mm + " mm")
+
+# Right
+print("Right")
+servo.rotate_servo(90)
+time.sleep(1)
+
+# Left
+print("Left")
+servo.rotate_servo(180)
+time.sleep(1)
+
+# Forward
+print("Forward")
+servo.rotate_servo(270)
+time.sleep(1)
+
 
 while True:
-        
-    # Read the sensor into variables
-    #mm = str(my_distance_sensor.read_mm())
-    inches = str(my_distance_sensor.read_inches())
-    distanceInches = float(inches)
 
-    # Print the values of the sensor to the console
-    print("Distance Sensor Reading: " + format(distanceInches) + " inches ") #+ mm + " mm")
-
-    # Right
-    print("Right")
-    servo.rotate_servo(90)
-    time.sleep(1)
-
-    # Left
-    print("Left")
-    servo.rotate_servo(180)
-    time.sleep(1)
-
-    # Forward
-    print("Forward")
-    servo.rotate_servo(270)
-    time.sleep(1)
     gpg.forward()
-    
 
     # Decision
     if distanceInches <= 10:
@@ -49,11 +50,9 @@ while True:
         gpg.forward()
     else:
         print("Keep on moving!")
-        gpg.forward() 
-           
+        gpg.forward()
 
     # sleep is only needed to see the measurements
     # sleep is blocking code, nothing else can happen during sleep
     # Don't use in production code
     # time.sleep(1)
-    
---
+++ Joann/servo.py
@@ -0,0 +1,42 @@
+#--------------------------------- IMPORTS -------------------------------------#
+import time                             # Import the time library for the sleep function
+import easygopigo3 as easy              # Import the GoPiGo3 library
+
+
+#--------------------------------- INITIALIZE GOPIGO -------------------------------------#
+gpg = easy.EasyGoPiGo3()          # Initialize EasyGoPiGo3 object
+servo = gpg.init_servo("SERVO1")  # Initialize servo object on Servo Port 1
+
+# Set servo pointing straight ahead at 90 degrees
+# You may have to change the degrees to adapt to your servo
+# All servos line up slightly differently
+servo.rotate_servo(90)
+print("Forward")
+time.sleep(1)
+
+
+#--------------------------------- MAIN PROGRAM -------------------------------------#
+def main():
+    # Right
+    print("Right")
+    servo.rotate_servo(30)
+    time.sleep(1)
+
+    # Left
+    print("Left")
+    servo.rotate_servo(150)
+    time.sleep(1)
+
+    # Forward
+    print("Forward")
+    servo.rotate_servo(90)
+    time.sleep(1)
+
+    # Disable or "float" the servo
+    servo.disable_servo()
+
+
+# If a standalone program, call the main function
+# Else, use as a module
+if __name__ == '__main__':
+    main()
