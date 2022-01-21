131bf035b3dd6b681a49f562500d67161332fec5
jonesj95wncc <80473876+jonesj95wncc@users.noreply.github.com>
2022-01-21 10:38:44 -0700

    Update functions2_obstacle_avoidance.py

---
+++ Joann/functions2_obstacle_avoidance.py
@@ -0,0 +1,15 @@
+"""
+    Name: functions2_obstacle_avoidance.py
+    Written by:
+    Written on:
+    Purpose:
+"""
+# Import time and the GoPiGo3 library
+import time
+import easygopigo3 as easy
+# Import tkinter for GUI
+from tkinter import * 
+# Override tk widgets with themed ttk widgets if available
+from tkinter.ttk import *
+# Used to exit the program 
+import sys
