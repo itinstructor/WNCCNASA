ec41ba7ed3548c4d4faeae3c3245915a93520942
Shelby James <hippiechick500@gmail.com>
2022-02-18 08:55:38 -0700

    .

--- Shelby/Facebook/Facebook_API.py
+++ Shelby/Facebook/Facebook_API.py
@@ -1,10 +1,19 @@
+# # Convert user token to page token
+# import requests
+
+page_id = "106479581966092"
+# f_access_token = "EAAJY4xlZCWQkBAJWeAXE4CirZCBFCbwUVPN55aWBPTQ1VG6M2NVgEUZAkPA5PThdlMfaeZBfh0fkM5Wc2ZAHq7ilyynSHjdARqdkv3bsGeoZAZBskRg9SVToe5YjvEJVivf7ZCQ2v7kojoSPG5FMuXX8UJ8dCXvzCIefMj07pKhpnsgn7BZBTdoSL"
+# r = requests.get(
+#     f"https://graph.facebook.com/{page_id}?fields=access_token={f_access_token}")
+
+# print(r.json())
+
+
 # Convert user token to page token
 import requests
 
-page_id = "106479581966092"
-f_access_token = "EAAJY4xlZCWQkBAIJMwMlSEq8LW8sC8U9yzxZCrMfLKgZByZBkJhIGOUtq1kN8CJWDoZBcY1andA7PNYJ62uZAOZB8oinemuGOoNpyAxg6cew02emH6c9qgR1M9wC0U2RCNNsnYpzlFTa0kDADTLDohpZC9DQ3JinFlNmdOTN5ApTOwZDZD"
+facebook_access_token = "EAAJY4xlZCWQkBAAekX9vCstTT9IIEgU8wfymrFb5KJnDdrCKxp8X4cjKXq4d6SaCLRKxbsGz15yL3ZBgrYrSXsKP1B1u3boMZABqAXTBnRZAgnHWzC5BLx5gLIS28DWAQN67Kl3TBzZCg35n3vn1COYQgLSclaGzVzHyznZCw8tBDZBnsXHRVKO"
 
-r = requests.get(
-    f"https://graph.facebook.com/{page_id}?fields=access_token={f_access_token}")
+r = requests.get(f"https://graph.facebook.com/{page_id}?fields=access_token&access_token={facebook_access_token}")
 
 print(r.json())
